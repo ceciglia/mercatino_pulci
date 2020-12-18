051dbef16ddece7094e32aa50742ccb9af8bed82
ceciglia <ceci.rossi99@gmail.com>
2020-12-18 13:39:18 +0100

    index-tendina-login

---
+++ js/tendinalogin.js
@@ -0,0 +1,10 @@
+//tendina login
+
+
+    function openNav() {
+        document.getElementById("myNav").style.height = "65%";
+    }
+
+    function closeNav() {
+        document.getElementById("myNav").style.height = "0%";
+    }
