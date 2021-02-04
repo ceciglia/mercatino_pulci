cdead8d30a3eab0b5a5eda583d13d37e428e6b35
AnnaFusari <anna.fusari99@gmail.com>
2021-02-04 16:27:54 +0100

    areageog

---
+++ areaGeografica.php
@@ -0,0 +1,23 @@
+<body>
+
+<form action="">
+  <select id="reg">
+    <option>Seleziona una regione:</option>
+  </select>
+</form>
+<br>
+<select id="prov">
+  <option>Seleziona prima una regione: </option>
+  <option>Nessuna</option>
+</select>
+<br>
+<br>
+<select id="com" >
+  <option>Seleziona prima una provincia: </option>
+  <option>Nessuna</option>
+</select>
+
+<script src="js/areaGeografica.js"></script>
+
+
+</body>
